73aecbac65ee9b820bfa4364912b4c0c6c4b5337
Daniel Tang <tang.d@hotmail.com>
2022-10-12 19:06:04 +1100

    Replace det_names with ifos, fix sigma_array calc

--- process_xml.py
+++ process_xml.py
@@ -25,20 +25,17 @@ def main(xml: str, out: str = "data"):
     (out_path / "snr_data").mkdir(exist_ok=True)  # make output subfolder for SNR data
 
     try:
-        det_names = list(xmlfile['snrs'].keys())
+        ifos = list(xmlfile['snrs'].keys())
     except KeyError as err:
         raise KeyError(
             f"snr array data not present {xml} file. Please check your coinc.xml!"
         ) from err
     
-    ndet = len(det_names)
+    ndet = len(ifos)
 
     # Calculate sigma = sqrt((h|h)) = deff/SNR
-    deff_array = []
-    max_snr_array = []
-    for det in det_names:
-        deff_array = np.append(deff_array, xmlfile['tables']['postcoh']['deff_'+det])
-        max_snr_array = np.append(max_snr_array, xmlfile['tables']['postcoh']['snglsnr_'+det])
+    deff_array = np.array([xmlfile['tables']['postcoh'][f"deff_{ifo}"] for ifo in ifos])
+    max_snr_array = np.array([xmlfile['tables']['postcoh'][f"snglsnr_{ifo}"] for ifo in ifos])
     sigma_array = deff_array*max_snr_array
 
     trigger_time = xmlfile["tables"]["postcoh"]["end_time"].item()
@@ -48,22 +45,22 @@ def main(xml: str, out: str = "data"):
     # trigger_time, ndet,    detcode1, ..., detcodeN,    max_snr1, ..., max_snrN,    sigma1, ..., sigmaN
     # 1+1+N+N+N = 3N+2 elements
     event_info = [trigger_time, ndet]
-    for det in det_names:
-        event_info.append(LAL_IFO_MAP[det])
+    for ifo in ifos:
+        event_info.append(LAL_IFO_MAP[ifo])
     event_info.append(max_snr_array)
     event_info.append(sigma_array)
     np.savetxt(out_path / 'event_info', np.array(event_info))
 
     # Save SNR series for each detector
-    for det in det_names:
-        timestamps = xmlfile["snrs"][det].index.values
-        real_snr = np.real(xmlfile['snrs'][det])
-        imag_snr = np.imag(xmlfile['snrs'][det])
-        snr_to_save = np.array([timestamps[det], real_snr, imag_snr]).T 
-        np.savetxt(out_path / "snr_data" / f"snr_det{LAL_IFO_MAP[det]}", snr_to_save)
+    for ifo in ifos:
+        timestamps = xmlfile["snrs"][ifo].index.values
+        real_snr = np.real(xmlfile['snrs'][ifo])
+        imag_snr = np.imag(xmlfile['snrs'][ifo])
+        snr_to_save = np.array([timestamps[ifo], real_snr, imag_snr]).T 
+        np.savetxt(out_path / "snr_data" / f"snr_det{LAL_IFO_MAP[ifo]}", snr_to_save)
 
     print(f'Trigger time: {trigger_time}')
-    print(f'Detectors: {det_names}')
+    print(f'Detectors: {ifos}')
     print(f'SNRs: {max_snr_array}')
     print(f'sigmas: {sigma_array}')
     print('SNR and event info have been saved. \n')
